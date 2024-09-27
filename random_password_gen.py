421222ae507205539166e3586149939e97f29264
MsRanaT <ranatayyeba@outlook.com>
2024-09-27 22:11:56 +0500

    password generator in python

---
+++ random_password_gen.py
@@ -0,0 +1,22 @@
+import string
+import random
+
+
+password=""
+All_pass=[]
+Pass_length=int(input("Enter length of password: "))
+
+char_values=string.ascii_letters+string.digits+string.punctuation  #adding all alphabets,numbers and punc through this module string
+x=0
+
+while x<5:
+    password=""
+    for i in range(Pass_length):
+        password+=(random.choice(char_values))
+    print(password)
+
+    All_pass.append(password)
+    x+=1
+
+print(All_pass)
+
